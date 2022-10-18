1b5d9a38f2f4e54a93f51b5fd73e76f838834f94
bojdavid <bojdavid80@gmail.com>
2022-10-18 20:50:48 +0100

    update 5

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,21 @@ void times_table(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
+		_putchar('0');
 		for (num2 = 0; num2 < 10; num2++)
 		{
 			mult = num1 * num2;
+			_putchar(',');
+			_putchar(' ');
+
 			if (mult < 10)
 			{
 				_putchar(mult + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
 			else
 			{
 				_putchar((mult / 10) + '0');
 				_putchar((mult % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
