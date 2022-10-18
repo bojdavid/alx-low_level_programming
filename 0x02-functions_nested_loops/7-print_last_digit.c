18afb809bd5f0d24aa17bb134521aebdf3b1b655
bojdavid <bojdavid80@gmail.com>
2022-10-18 23:34:23 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
+
 /**
-  *print_last_digit - prints the alphabet, in lowercase, followed by a new line.
+  *print_last_digit - prints the alphabet, in lowercase
   *a: int
   *Return: the asbsolute val of the int
   */
 
 int print_last_digit(int n)
 {
-	int ld = n % 10;
-	if (ld < 0)
-		ld *= -1;
-	_putchar(ld + '0');
-	return (ld);
+	int l_d = n % 10;
+
+	if (l_d < 0)
+		l_d *= -1;
+	_putchar(l_d + '0');
+	return (l_d);
 }
