9039ad345eea4a485e465c76c799e3cb96b98dff
bojdavid <bojdavid80@gmail.com>
2022-10-18 13:13:07 +0100

    task 6..

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,17 +4,13 @@ int _abs(int a);
 
 /**
   *_abs - prints the alphabet, in lowercase, followed by a new line.
-  *a: int
+  *@a: int
   *Return: the asbsolute val of the int
   */
 int _abs(int a)
 {
 	if (a >= 0)
-	{
 		return (a);
-	}
-	else if (a < 0)
-	{
+	else
 		return (-a);
-	}
 }
