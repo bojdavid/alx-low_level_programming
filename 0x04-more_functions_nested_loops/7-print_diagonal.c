8f4c49b763edb25c8d0c4435824596dd8233c9b5
bojdavid <bojdavid80@gmail.com>
2022-10-20 15:06:23 +0100

    corrections

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,16 @@ void print_diagonal(int n)
 {
 	int count, space;
 
-	if (n > 0)
-	{
-		for (count = 1; count <= n; count++)
-		{
-			for (space = 1; space <= count; space++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
-	else
+	if (n <= 0)
 		_putchar('\n');
+
+	for (count = 1; count <= n; count++)
+	{
+		for (space = 1; space <= count; space++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
