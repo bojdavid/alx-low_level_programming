87d868e6ee651cef007330eaa5784be97df6d591
bojdavid <bojdavid80@gmail.com>
2022-10-30 04:49:53 +0100

    one

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,11 +16,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && (src + 1); i++)
 	{
-		dest[i] = src[i];
+		*dest[i] = *src[i];
 	}
 	for (; i < n; i++)
 	{
-		dest[i] = '\0';
+		*dest[i] = '\0';
 	}
 	return (dest);
 }
