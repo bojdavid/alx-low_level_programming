9cec20dd17f223d42a210d0b592264de6900d340
bojdavid <bojdavid80@gmail.com>
2022-11-05 16:37:01 +0100

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - print file name
+  *@argc: no of args
+  *@argv: vals of args
+  *Return: 0 on success
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
